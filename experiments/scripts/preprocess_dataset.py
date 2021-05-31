7ecf2b779c8beea85ca5eecc4f2d4772d87fa6d9
ktagowski <kamil.tagowski@gmail.com>
2021-05-31 10:41:09 +0200

    Add dgl to preprocess

--- experiments/scripts/preprocess_dataset.py
+++ experiments/scripts/preprocess_dataset.py
@@ -2,7 +2,7 @@ import yaml
 
 from src.dataset import load_dataset
 from src.utils import get_module_from_str
-
+from ogb.lsc import PygPCQM4MDataset, DglPCQM4MDataset
 
 def main():
     with open("params.yaml", "r") as f:
@@ -11,7 +11,8 @@ def main():
     s2g_converter = get_module_from_str(cfg["smiles2graph"]["cls"])(
         **cfg["smiles2graph"]["args"]
     )
-    load_dataset(smiles2graph_fn=s2g_converter)
+    load_dataset(PygPCQM4MDataset, smiles2graph_fn=s2g_converter)
+    load_dataset(DglPCQM4MDataset, smiles2graph_fn=s2g_converter)
 
 
 if __name__ == "__main__":
